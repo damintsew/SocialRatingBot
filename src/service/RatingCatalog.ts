19edb186a3124b9c94764e0dcb9da66d988ebdb8
Andrei Damintsev <adamintsev@devexperts.com>
2022-08-12 11:05:47 +0300

    Something working + Dockerfile

--- src/service/RatingCatalog.ts
+++ src/service/RatingCatalog.ts
@@ -3,7 +3,8 @@ export class RatingCatalog {
     static catalog = {
         "Xicompartiya_by_fStikBot": {
             "AgADCQ8AAmSY6Uo": {ratingValue: 20, name: "Виннипух +20 к рейтингу"},
-            "AgAD4gwAAvgb4Uo": {ratingValue: -20, name: "Виннипух -20 к рейтингу"}
+            "AgAD4gwAAvgb4Uo": {ratingValue: -20, name: "Виннипух -20 к рейтингу"},
+            "AgADvQwAAlI04Eo": {ratingValue: +15, name: "Желтая морда с усами"}
         }
     }
 }
